2df5aee97a6315fb88088004e9d710856bedd80d
ahmed-mohadin <ahmed.mohadin@outlook.com>
2022-09-23 21:32:44 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
+
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var YOUR_MONGO_UR = 'YOUR_MONGO_UR_HERE';
+var mongoDB = process.env.MONGO_UR || YOUR_MONGO_UR;
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to db');
+  });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var membersRouter = require('./routes/members');
 
 var app = express();
 
@@ -13,22 +32,73 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// PassportJS setup
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      // No user
+      if (!user) {
+        app.locals.loginErrors = {
+          usernameError: `Username ${username} not found`,
+        };
+        return done(null, false);
+      }
+      // Check password match
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // Passwords match
+          return done(null, user);
+        } else {
+          // Passwords don't match
+          app.locals.loginErrors = { passwordError: 'Incorrect password' };
+          return done(null, false);
+        }
+      });
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/members-only', membersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
